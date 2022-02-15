4dd957521536b49945e485ea38a78743aca20d96
Alejandro Hincapie <58157472+Rokneer@users.noreply.github.com>
2022-02-15 07:41:29 -0500

    Add files via upload

---
+++ mover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 acceleration;
+
+    private Vector3 position, velocity;
+
+    public void Move()
+    {
+        position += velocity * Time.deltaTime;
+        if ((position.x >= 5) || (position.x <= -5))
+        {
+            velocity.x = velocity.x * -1;
+        }
+        if ((position.y >= 5) || (position.y <= -5))
+        {
+            velocity.y = velocity.y * -1;
+        }
+        velocity = acceleration * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void Draw()
+    {
+        Debug.DrawLine(transform.position, velocity, Color.blue);
+        Debug.DrawLine(transform.position, position, Color.red);
+        //Debug.DrawLine(transform.position, acceleration, Color.green);
+    }
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    void Update()
+    {
+        Move();
+        Draw();
+    }
+}
